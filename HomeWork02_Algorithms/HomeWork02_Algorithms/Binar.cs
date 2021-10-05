0f0f7ca640abd6fbf30800c60adf72314ac84434
Artur <artur071982@gmail.com>
2021-10-05 11:22:25 +0500

    Binar seach

---
+++ HomeWork02_Algorithms/HomeWork02_Algorithms/Binar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doublyLinkedList
+{
+    class Binar
+    {
+        public  int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ HomeWork02_Algorithms/HomeWork02_Algorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var numArra = new int[] { 55, 11, 77, 22, 16, 57, 56, 14 };
+            Console.Write("Введите искомое число: ");
+            int seachNum = Convert.ToInt32(Console.ReadLine());
+           Binar binar = new Binar();
+           Console.WriteLine( $"число {seachNum} является элементом {binar.BinarySearch(numArra, seachNum)} массива" );
+           Console.ReadLine();
+        }
+    }
+}
